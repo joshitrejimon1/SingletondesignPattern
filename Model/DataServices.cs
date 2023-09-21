b601a6d10e956cc2db3d7496e3ceea6b22d0f7ba
joshitrejimon1 <joshit.rejimon@aurionpro.com>
2023-09-21 11:22:22 +0530

    Add project files.

---
+++ Model/DataServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletondesignPattern.Model
+{
+    internal class DataServices
+    {
+        private static DataServices? instance;
+
+        DataServices()
+        {
+            Console.WriteLine("DATA SERVICES IS CREATED ");
+
+        }
+
+        public void DoProcess()
+        {
+            Console.WriteLine("PROCESSING IS DONE"+GetHashCode());
+        }
+
+        public static DataServices GetInstance()
+        {
+            if (instance == null)
+                instance = new DataServices();
+            return
+                instance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using SingletondesignPattern.Model;
+
+namespace SingletondesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataServices d1 = DataServices.GetInstance();
+            d1.DoProcess();
+
+            DataServices d2 = DataServices.GetInstance();
+            d2.DoProcess();
+
+        }
+    }
+}
